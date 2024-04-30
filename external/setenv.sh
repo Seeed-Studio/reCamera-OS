599453394c0dee6c2b33497b6b8317c33defa6d1
mingzq <north_sea@qq.com>
2024-04-30 08:31:35 +0000

    external: sg2002_recamera_sd_defconfig

--- external/setenv.sh
+++ external/setenv.sh
@@ -21,7 +21,7 @@ rsync_dir ./linux_5.10
 rsync_dir ./ramdisk
 rsync_dir ./osdrv
 rsync_dir ./middleware
-rsync_dir ./isp_tuning
+# rsync_dir ./isp_tuning
 rsync_dir ./oss
 rsync_dir ./SensorSupportList/ middleware/v2/component/isp/
 rsync_dir ./buildroot-2021.05
@@ -30,7 +30,9 @@ rsync_dir ./buildroot-2021.05
 # patch externals
 ###################################
 rsync_dir $EXTERNAL/build .
-# rsync_dir $EXTERNAL/rootfs_overlay/ buildroot-2021.05/board/cvitek/CV181X/overlay/
+rsync_dir $EXTERNAL/buildroot/ buildroot*/
+rsync_dir $EXTERNAL/isp_tuning .
+rsync_dir $EXTERNAL/rootfs_overlay/ buildroot-2021.05/board/cvitek/CV181X/overlay/
 
 # patches=`find $EXTERNAL/patches/ -name "*.patch" | sort`
 # for patch in ${patches}; do
@@ -41,12 +43,17 @@ rsync_dir $EXTERNAL/build .
 ln -sf $TOPDIR/host-tools $PROJECT_OUT/
 
 ###################################
+# modify build/Makefile
+###################################
+sed -i 's/${Q}$(MAKE) -C $(BR_DIR).*/${Q}$(MAKE) -C $(BR_DIR) $(BR_DEFCONFIG) BR2_TOOLCHAIN_EXTERNAL_PATH=$(CROSS_COMPILE_PATH) O=$(TARGET_OUTPUT_DIR)/' \
+    $PROJECT_OUT/build/Makefile
+sed -i 's/${Q}$(MAKE) -j${NPROC} -C $(BR_DIR).*/${Q}$(MAKE) -j${NPROC} -C $(BR_DIR) O=$(TARGET_OUTPUT_DIR)/' \
+    $PROJECT_OUT/build/Makefile
+
+###################################
 # modify cvisetup.sh
 ###################################
-# sed -i 's/TOOLCHAIN_PATH=.*/TOOLCHAIN_PATH="\$TOPDIR"\/host-tools/' $PROJECT_OUT/build/cvisetup.sh
-# echo "INFO: Change TOOLCHAIN_PATH to \"$TOPDIR/host-tools in cvisetup.sh"\"
-
-sed -i 's/^TOP_DIR=\$\(.*\)/TOP_DIR=\$PROJECT_OUT/' $PROJECT_OUT/build/cvisetup.sh
+sed -i 's/^TOP_DIR=$(.*)/TOP_DIR=$PROJECT_OUT/' $PROJECT_OUT/build/cvisetup.sh
 echo "INFO: Change TOP_DIR to \"$PROJECT_OUT in cvisetup.sh"\"
 
 sed -i 's/CVI_TARGET_PACKAGES_LIBDIR=.*/CVI_TARGET_PACKAGES_LIBDIR=$(make --no-print-directory print-target-packages-libdir)/' \
@@ -60,4 +67,3 @@ source $PROJECT_OUT/build/cvisetup.sh
 ###################################
 # overwrite cvisetup.sh functions
 ###################################
-
