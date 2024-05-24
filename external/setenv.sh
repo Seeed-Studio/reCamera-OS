7c3da846123ead54becea61bdb9967700365c689
mingzq <north_sea@qq.com>
2024-05-24 07:26:16 +0000

    uboot for emmc

--- external/setenv.sh
+++ external/setenv.sh
@@ -31,8 +31,9 @@ rsync_dir ./buildroot-2021.05
 ###################################
 rsync_dir $EXTERNAL/build .
 rsync_dir $EXTERNAL/buildroot/ buildroot*/
-rsync_dir $EXTERNAL/ramdisk/ ramdisk/
 rsync_dir $EXTERNAL/isp_tuning .
+rsync_dir $EXTERNAL/ramdisk/ ramdisk/
+rsync_dir $EXTERNAL/u-boot/ u-boot*/
 # rsync_dir $EXTERNAL/rootfs_overlay/ buildroot*/board/cvitek/CV181X/overlay/
 
 rsync -av --delete $EXTERNAL/buildroot/package/nodejs/ $PROJECT_OUT/buildroot*/package/nodejs/
