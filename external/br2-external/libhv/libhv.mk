0600d59b3bb76151bec4d618ad0a8dbc2d4886b8
Lesords <2385342343@qq.com>
2024-08-30 03:32:12 +0000

    chore: add libhv compilation rules in buildroot

---
+++ external/br2-external/libhv/libhv.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# libhv
+#
+################################################################################
+
+
+LIBHV_VERSION = v1.3.3
+LIBHV_SOURCE = $(LIBHV_VERSION).tar.gz
+LIBHV_SITE = https://github.com/ithewei/libhv/archive/refs/tags
+LIBHV_INSTALL_STAGING = YES
+
+define LIBHV_CONFIGURE_CMDS
+    (cd $(@D); $(TARGET_CONFIGURE_OPTS) ./configure)
+endef
+
+define LIBHV_BUILD_CMDS
+    $(TARGET_CONFIGURE_OPTS) $(MAKE) libhv -C $(@D)
+endef
+
+define LIBHV_INSTALL_TARGET_CMDS
+	cp -rf $(@D)/lib/* $(TARGET_DIR)/usr/lib/
+endef
+
+define LIBHV_INSTALL_STAGING_CMDS
+	cp -rf $(@D)/lib/* $(STAGING_DIR)/usr/lib/
+endef
+
+$(eval $(generic-package))
