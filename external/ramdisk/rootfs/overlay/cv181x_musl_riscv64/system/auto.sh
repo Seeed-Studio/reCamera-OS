150168a5b4cfbde92ce45fa8e11bb147f3ca5c52
mingzq <north_sea@qq.com>
2024-08-30 09:24:09 +0000

    feat: auto mount userdata partition

--- external/ramdisk/rootfs/overlay/cv181x_musl_riscv64/system/auto.sh
+++ external/ramdisk/rootfs/overlay/cv181x_musl_riscv64/system/auto.sh
@@ -4,12 +4,33 @@ ${CVI_SHOPTS}
 export LD_LIBRARY_PATH="/lib:/lib/3rd:/lib/arm-linux-gnueabihf:/usr/lib:/usr/local/lib:/mnt/system/lib:/mnt/system/usr/lib:/mnt/system/usr/lib/3rd:/mnt/data/lib"
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/mnt/system/usr/bin:/mnt/system/usr/sbin:/mnt/data/bin:/mnt/data/sbin"
 
-DEFAULT_APP=/mnt/system/default_app
-
+# boot success, clean boot_cnt
 if [ $(fw_printenv boot_cnt) != "boot_cnt=0" ]; then
     fw_setenv boot_cnt 0;
 fi
 
+# mount userdata partition
+USERDATA_PARTITION="/dev/mmcblk0p6"
+USERDATA_MOUNTPOINT="/userdata"
+fs_type=$(blkid -o value -s TYPE $USERDATA_PARTITION)
+echo "userdata partition type: $fs_type"
+if [ "$fs_type" == "" ]; then
+   mkfs.ext4 $USERDATA_PARTITION
+fi
+fs_type=$(blkid -o value -s TYPE $USERDATA_PARTITION)
+if [ "$fs_type" != "" ]; then
+   if [ ! -d $USERDATA_MOUNTPOINT ]; then
+      mkdir -p $USERDATA_MOUNTPOINT
+   fi
+   mount $USERDATA_PARTITION $USERDATA_MOUNTPOINT
+   result=$(mount | grep $USERDATA_MOUNTPOINT)
+   if [ "$result" == "" ]; then
+      echo "mount $USERDATA_PARTITION to $USERDATA_MOUNTPOINT failed"
+   fi
+fi
+
+# default app
+DEFAULT_APP=/mnt/system/default_app
 if [ ! -f "/tmp/evb_init" ];then
    echo 1 > /tmp/evb_init
    if [ -x $DEFAULT_APP ]; then
