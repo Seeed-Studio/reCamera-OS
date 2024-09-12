6c0912e7e3385de1281d9a7d07a49e28f68bab29
mingzq <north_sea@qq.com>
2024-09-12 04:31:46 +0000

    fix: correct env

--- external/ramdisk/rootfs/overlay/cv181x_musl_riscv64/system/auto.sh
+++ external/ramdisk/rootfs/overlay/cv181x_musl_riscv64/system/auto.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 ${CVI_SHOPTS}
 
-export LD_LIBRARY_PATH="/lib:/lib/3rd:/lib/arm-linux-gnueabihf:/usr/lib:/usr/local/lib:/mnt/system/lib:/mnt/system/usr/lib:/mnt/system/usr/lib/3rd:/mnt/data/lib"
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/mnt/system/usr/bin:/mnt/system/usr/sbin:/mnt/data/bin:/mnt/data/sbin"
-
 # boot success, clean boot_cnt
 if [ $(fw_printenv boot_cnt) != "boot_cnt=0" ]; then
     fw_setenv boot_cnt 0;
