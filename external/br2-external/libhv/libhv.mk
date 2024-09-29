6aa276bac53337ad8ed8ffd44de2e5f4e1493891
Lesords <2385342343@qq.com>
2024-09-29 12:28:40 +0000

    chore: open the https service in the libhv library

--- external/br2-external/libhv/libhv.mk
+++ external/br2-external/libhv/libhv.mk
@@ -4,18 +4,18 @@
 #
 ################################################################################
 
-
 LIBHV_VERSION = v1.3.3
 LIBHV_SOURCE = $(LIBHV_VERSION).tar.gz
 LIBHV_SITE = https://github.com/ithewei/libhv/archive/refs/tags
+LIBHV_DEPENDENCIES = openssl
 LIBHV_INSTALL_STAGING = YES
 
 define LIBHV_CONFIGURE_CMDS
-    (cd $(@D); $(TARGET_CONFIGURE_OPTS) ./configure --with-mqtt)
+	(cd $(@D); $(TARGET_CONFIGURE_OPTS) ./configure --with-mqtt --with-openssl)
 endef
 
 define LIBHV_BUILD_CMDS
-    $(TARGET_CONFIGURE_OPTS) $(MAKE) libhv -C $(@D)
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) libhv -C $(@D)
 endef
 
 define LIBHV_INSTALL_TARGET_CMDS
