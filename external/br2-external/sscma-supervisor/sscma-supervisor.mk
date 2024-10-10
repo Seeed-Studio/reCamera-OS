6a6af8009f4342b3dd99feb3350d4ae093142b4c
Lesords <2385342343@qq.com>
2024-10-10 02:39:05 +0000

    fix: solve the problem that the compiler could not be found when compiling supervisor

--- external/br2-external/sscma-supervisor/sscma-supervisor.mk
+++ external/br2-external/sscma-supervisor/sscma-supervisor.mk
@@ -10,15 +10,15 @@ BR_NO_CHECK_HASH_FOR += $(SSCMA_SUPERVISOR_SOURCE)
 
 define SSCMA_SUPERVISOR_EXTRACT_CMDS
 	$(UNZIP) -d $(@D) $(SSCMA_SUPERVISOR_DL_DIR)/$(SSCMA_SUPERVISOR_SOURCE)
-    @mv -fv $(@D)/* $(@D)/source
+	@mv -fv $(@D)/* $(@D)/source
 endef
 
 define SSCMA_SUPERVISOR_BUILD_CMDS
-	cd $(@D)/source && ./build.sh
+	(cd $(@D)/source; $(TARGET_CONFIGURE_OPTS) ./build.sh)
 endef
 
 define SSCMA_SUPERVISOR_INSTALL_TARGET_CMDS
-    @cp -rf $(@D)/source/install/* $(TARGET_DIR)/
+	@cp -rf $(@D)/source/install/* $(TARGET_DIR)/
 endef
 
 $(eval $(generic-package))
