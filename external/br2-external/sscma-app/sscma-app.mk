409cf3e7e7030ceb0a62b562874d431ccfda65fa
Lesords <2385342343@qq.com>
2024-10-11 12:22:44 +0000

    chore: download the node-red-contrib-sscma node through npm in buildroot

--- external/br2-external/sscma-app/sscma-app.mk
+++ external/br2-external/sscma-app/sscma-app.mk
@@ -9,7 +9,7 @@ SSCMA_APP_SITE = https://github.com/Seeed-Studio/sscma-example-sg200x.git
 SSCMA_APP_SITE_METHOD = git
 SSCMA_APP_GIT_SUBMODULES = YES
 SSCMA_APP_LICENSE = Apache-2.0
-SSCMA_APP_DEPENDENCIES = mosquitto
+SSCMA_APP_DEPENDENCIES = mosquitto host-nodejs
 
 define SSCMA_APP_BUILD_CMDS
 	(cd $(@D); \
@@ -21,6 +21,8 @@ endef
 
 define SSCMA_APP_INSTALL_TARGET_CMDS
 	@cp -rf $(@D)/install/* $(TARGET_DIR)/
+	mkdir -p $(TARGET_DIR)/home/recamera/.node-red/node_modules
+	$(NPM) install --no-audit --no-update-notifier --no-fund --save --save-prefix=~ --production --engine-strict --prefix $(TARGET_DIR)/home/recamera/.node-red node-red-contrib-sscma@0.0.5
 endef
 
 $(eval $(generic-package))
