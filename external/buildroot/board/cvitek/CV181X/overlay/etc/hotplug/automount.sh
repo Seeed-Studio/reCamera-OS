36e7a8dcfddd886a1c60f813eba7f1a250bb2bde
mingzq <north_sea@qq.com>
2024-12-27 06:24:40 +0000

    udev: sd auto mount

--- external/buildroot/board/cvitek/CV181X/overlay/etc/hotplug/automount.sh
+++ external/buildroot/board/cvitek/CV181X/overlay/etc/hotplug/automount.sh
@@ -1,34 +1,24 @@
 #!/bin/sh
 
-function umount_dir() {
-  if [ -d $1 ]; then
-      umount $1
-      rm -rf $1
-  fi
-}
-
 MOUNT_POINT=/tmp/sd
 LINK_POINT=/mnt/sd
 
-# echo "$1 $2"
+# echo "<<<<<<<<<<<<<$1 $2>>>>>>>>>>>>>"
 if [ "$1" == "mmcblk1p1" ]; then
+  disk="/dev/$1"
   if [ "$2" = "add" ]; then
-      #dir=$(mktemp -d)
-      dir=$MOUNT_POINT
-      umount_dir $dir
-      mkdir -p $dir
-      # chown -R recamera:recamera $dir
-      mount $1 $dir
+      mkdir -p $MOUNT_POINT
+      umount $MOUNT_POINT
+      mount $disk $MOUNT_POINT
 
       rootfs_rw on
       if [ -L $LINK_POINT ]; then
         rm $LINK_POINT
       fi
-      ln -s $dir /mnt/
+      ln -s $MOUNT_POINT /mnt/
       rootfs_rw off
   else
-      dir=$(realpath $LINK_POINT)
-      umount_dir $dir
+      umount $MOUNT_POINT
       rootfs_rw on
       rm $LINK_POINT
       rootfs_rw off
