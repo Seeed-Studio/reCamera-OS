b4176df1cce7869a0d5d24f550bbdfd15b9ac28e
LynnL4 <lht856@foxmail.com>
2024-12-27 09:00:24 +0000

    chore: update sscma-node & sscma-supervisor

--- external/br2-external/sscma-node/sscma-node.mk
+++ external/br2-external/sscma-node/sscma-node.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SSCMA_NODE_VERSION = cae50db44c52729aee624cabb564a4b14e154d8f
+SSCMA_NODE_VERSION = d975f51f18899569cdeab2528db463c5c8ab9c03
 SSCMA_NODE_SITE = https://github.com/Seeed-Studio/sscma-example-sg200x
 SSCMA_NODE_SITE_METHOD = git
 SSCMA_NODE_GIT_SUBMODULES = YES
--- external/br2-external/sscma-supervisor/sscma-supervisor.mk
+++ external/br2-external/sscma-supervisor/sscma-supervisor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SSCMA_SUPERVISOR_VERSION = cae50db44c52729aee624cabb564a4b14e154d8f
+SSCMA_SUPERVISOR_VERSION = d975f51f18899569cdeab2528db463c5c8ab9c03
 SSCMA_SUPERVISOR_SITE = https://github.com/Seeed-Studio/sscma-example-sg200x
 SSCMA_SUPERVISOR_SITE_METHOD = git
 SSCMA_SUPERVISOR_GIT_SUBMODULES = YES
