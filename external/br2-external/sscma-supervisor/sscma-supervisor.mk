ed97b3b05f7fafe6bd959fbf7061bd3910995326
LynnL4 <lht856@foxmail.com>
2025-03-25 11:17:55 +0000

    chore: update sscma-node & supervisor 0.2.0

--- external/br2-external/sscma-supervisor/sscma-supervisor.mk
+++ external/br2-external/sscma-supervisor/sscma-supervisor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SSCMA_SUPERVISOR_VERSION = 0.1.6
+SSCMA_SUPERVISOR_VERSION = 0.2.0
 SSCMA_SUPERVISOR_SITE = https://github.com/Seeed-Studio/sscma-example-sg200x
 SSCMA_SUPERVISOR_SITE_METHOD = git
 SSCMA_SUPERVISOR_GIT_SUBMODULES = YES
