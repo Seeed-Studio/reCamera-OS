89d24f67cf269f4858928d8810e36845298ab784
mingzq <north_sea@qq.com>
2025-05-08 03:15:26 +0000

    feat: default disable sshd (install sshd into /etc/init.d/disabled)

--- external/buildroot/package/openssh/openssh.mk
+++ external/buildroot/package/openssh/openssh.mk
@@ -130,7 +130,7 @@ endef
 
 define OPENSSH_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 755 package/openssh/S50sshd \
-		$(TARGET_DIR)/etc/init.d/S50sshd
+		$(TARGET_DIR)/etc/init.d/disabled/S50sshd
 endef
 endif
 
