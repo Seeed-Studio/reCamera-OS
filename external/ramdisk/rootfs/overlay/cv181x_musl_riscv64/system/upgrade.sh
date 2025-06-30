02e6a0b3e2c83a40d9d1b2e4de5164bfc41ee682
mingzq <north_sea@qq.com>
2025-06-30 06:16:25 +0000

    fix: upgrade.sh

--- external/ramdisk/rootfs/overlay/cv181x_musl_riscv64/system/upgrade.sh
+++ external/ramdisk/rootfs/overlay/cv181x_musl_riscv64/system/upgrade.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+OFFICIAL_URL="https://github.com/Seeed-Studio/reCamera-OS/releases/latest"
 MD5_FILE=sg2002_recamera_emmc_md5sum.txt
 URL_FILE=url.txt
 
@@ -7,21 +9,22 @@ BOOT_PART=/dev/mmcblk0p1
 RECV_PART=/dev/mmcblk0p5
 ROOTFS=/dev/mmcblk0p3
 ROOTFS_B=/dev/mmcblk0p4
-ROOTFS_FILE=rootfs_ext4.emmc
 
+UPGRADE_TMP=/userdata/.upgrade
 UPGRADE_FILES=/tmp/upgrade
-mkdir -p "$UPGRADE_FILES"
+mkdir -p "$UPGRADE_FILES" "$UPGRADE_TMP"
 
-USERDATA=/userdata
-UPGRADE_TMP=$USERDATA/.upgrade
-mkdir -p "$UPGRADE_TMP"
-
+# global variable
+RunCase=""
+ResultFile=""
 MountPath=""
 Step=0
-log_step() {
+step_log() {
     let Step++
     echo "Step$Step: $1"
 }
+
+step_result() { echo "Result: $1"; }
 
 # exit
 cleanup() {
@@ -30,29 +33,27 @@ cleanup() {
         umount "$MountPath" 2>/dev/null
         ! mount | grep -qw "$MountPath" && rm -rf "$MountPath"
     }
-    rm -rf "$UPGRADE_FILES/$RUN_CASE.mutex"
+    rm -rf "$UPGRADE_FILES/$RunCase.mutex" 2>/dev/null
 }
-trap cleanup SIGINT SIGTERM
+trap cleanup SIGINT SIGTERM EXIT
 
 exit_upgrade() {
     cleanup
-    local file_result="$UPGRADE_FILES/$RUN_CASE"
-    [ -n "$RUN_CASE" ] || file_result="$UPGRADE_FILES/result"
+    [ -n "$RunCase" ] || ResultFile="$UPGRADE_FILES/result"
     [ -z "$1" ] && {
-        [ -f "$file_result" ] || echo "Success" >"$file_result"
+        [ -f "$ResultFile" ] || echo "Success" >"$ResultFile"
         echo "Success"
         exit 0
     }
-    echo "Failed" >"$file_result"
-    echo "Failed: $1"
+    echo "Failed: $1" | tee "$ResultFile"
     exit 1
 }
 
 # ps
 ps_mutex() {
-    local file="$UPGRADE_FILES/$RUN_CASE"
+    local file="$UPGRADE_FILES/$RunCase"
     [ -f "$file.mutex" ] && {
-        echo "./upgrade.sh $RUN_CASE is running."
+        echo "./upgrade.sh $RunCase is running."
         exit 1
     }
     Step=0
@@ -60,15 +61,45 @@ ps_mutex() {
     echo $$ >"$file.mutex"
 }
 
+kill_ps() {
+    local pid=$(ps | grep "$1" | grep -v grep | awk '{print $1}')
+    [ -n "$pid" ] && kill -9 "$pid"
+}
+
 ps_stop() {
-    local pid=$(cat "$UPGRADE_FILES/$RUN_CASE.mutex")
-    [ -n "$pid" ] && {
-        kill -9 $(($pid))
-    }
+    kill_ps "dd of=$ROOTFS bs=1M status=progress"
+    kill_ps "dd of=$ROOTFS_B bs=1M status=progress"
+    killall "$(basename $0)"
+    echo "stopped"
     exit 0
 }
 
 # utils
+parse_md5() { # sg2002_recamera_emmc_md5sum.txt
+    local info=$(grep ".*ota.zip" "$2" 2>/dev/null)
+    [ -z "$info" ] && return 1
+    case "$1" in
+    name) echo "$info" | awk '{print $2}' ;;
+    md5) echo "$info" | awk '{print $1}' ;;
+    os) echo "$info" | awk '{print $2}' | cut -d'_' -f2 ;;
+    version) echo "$info" | awk '{print $2}' | cut -d'_' -f3 ;;
+    *) return 1 ;;
+    esac
+}
+
+zip_get_size() { # zip=$1 file=$2
+    local size=$(unzip -l "$1" 2>/dev/null | grep "$2" | awk '{print $1}')
+    [ -z "$size" ] && return 1
+    echo $((size))
+}
+
+zip_read_md5() { # md5sum.txt
+    zip=$1 file=$2
+    md5=$(unzip -p "$zip" md5sum.txt 2>/dev/null | grep "$file" | awk '{print $1}')
+    [ -z "$md5" ] && return 1
+    echo $md5
+}
+
 mount_recovery() {
     local fs_type=$(blkid -o value -s TYPE "$RECV_PART")
     [ "$fs_type" != "ext4" ] && {
@@ -81,25 +112,25 @@ mount_recovery() {
     exit_upgrade "mount $RECV_PART on $MountPath failed."
 }
 
-is_use_partition_b() {
-    local root_dev=$(mountpoint -n / | awk '{print $1}')
+is_rootfs_b() {
+    local root_dev=$(mountpoint -n / | awk '{print $1}') || exit_upgrade "get rootfs"
     [ "$root_dev" = "$(realpath "$ROOTFS_B")" ] && return 1 || return 0
 }
 
-write_upgrade_flag() {
-    fw_setenv use_part_b $1 || exit_upgrade "write use_part_b"
-    fw_setenv boot_cnt 0 || exit_upgrade "write boot_cnt"
-    fw_setenv boot_failed_limits 5 || exit_upgrade "write boot_failed_limits"
-    fw_setenv boot_rollback || exit_upgrade "write boot_rollback"
+set_bootenv() {
+    fw_setenv "$1" "$2" || exit_upgrade "set $1=$2"
 }
 
 switch_partition() {
-    log_step "Switch rootfs partition"
-    is_use_partition_b
-    local is_part_b=$?
-    [ $is_part_b -eq 0 ] && write_upgrade_flag 1
-    [ $is_part_b -eq 1 ] && write_upgrade_flag 0
-    log_step "Please reboot to take effect."
+    step_log "Switch rootfs partition"
+    is_rootfs_b
+    local part_b=$?
+    part_b=$((1 - part_b))
+    set_bootenv use_part_b $part_b
+    set_bootenv boot_cnt 0
+    set_bootenv boot_failed_limits 5
+    set_bootenv boot_rollback 1
+    step_log "Please reboot to take effect."
 }
 
 get_upgrade_url() {
@@ -111,122 +142,103 @@ get_upgrade_url() {
         full_url="$url/$MD5_FILE"
     }
     echo "$full_url"
-    return 0
 }
 
 wget_file() {
-    url=$1 file=$2
-    log_step "Get file size $url"
-    size="$(wget --no-check-certificate --spider "$1" 2>&1 | grep 'Length' | awk '{print $2}')" || exit_upgrade "get size $url"
-    echo "size=$((size))"
+    local url=$1 file=$2 size
+    step_log "Download $url"
 
-    log_step "Download $url"
+    size="$(wget --no-check-certificate --spider "$1" 2>&1 | grep 'Length' | awk '{print $2}')"
+    [ -z "$size" ] && exit_upgrade "get size $url"
+    echo "$file" >"$ResultFile.file"
+    echo "$size" >"$ResultFile.size"
+
     wget -q -c -T 10 -t 3 --no-check-certificate --show-progress "$url" -O "$file" || exit_upgrade "download $url"
     [ ! -s "$file" ] && exit_upgrade "download $file is empty"
 }
 
-dd_calc_md5() {
-    local file=$1 size=$2
-    [ -e $file ] || return 1
-    [ -z $size ] && size=$(stat -c %s "$file" 2>/dev/null)
-    [ $((size)) -eq 0 ] && return 1
-    local md5=$(dd if="$file" bs=1M 2>/dev/null | head -c "$size" | md5sum | awk '{print $1}')
-    [ -z "$md5" ] && return 1
-    echo "$md5"
-}
-
-parse_md5() { # sg2002_recamera_emmc_md5sum.txt
-    local info=$(grep ".*ota.zip" "$2" 2>/dev/null)
-    [ -z "$info" ] && return 1
-    [ "$1" = "name" ] && {
-        echo "$info" | awk '{print $2}'
-        return 0
-    }
-    [ "$1" = "md5" ] && {
-        echo "$info" | awk '{print $1}'
-        return 0
-    }
-    [ "$1" = "os" ] && {
-        echo $(echo "$info" | awk '{print $2}' | cut -d'_' -f2)
-        return 0
-    }
-    [ "$1" = "version" ] && {
-        echo $(echo "$info" | awk '{print $2}' | cut -d'_' -f3)
-        return 0
-    }
-    return 1
-}
-
-zip_get_size() { # zip=$1 file=$2
-    local size=$(unzip -l "$1" 2>/dev/null | grep "$2" | awk '{print $1}')
-    [ -z "$size" ] && return 1
-    echo $((size))
-}
-
-zip_read_md5() {
-    zip=$1 file=$2
-    md5=$(unzip -p "$zip" md5sum.txt 2>/dev/null | grep "$file" | awk '{print $1}')
-    [ -z "$md5" ] && return 1
-    echo $md5
-}
-
-zip_calc_md5() {
-    zip=$1 file=$2
-    md5=$(unzip -p $zip $file 2>/dev/null | md5sum | awk '{print $1}')
-    [ -z "$md5" ] && return 1
-    echo $md5
-}
-
 zip_write_part() {
     local zip=$1 part=$2 file=$3
-    log_step "Write $part with $file"
-    unzip -p $zip $file 2>/dev/null >$part
-    [ $? -ne 0 ] && exit_upgrade "write $part"
+    local size read_md5 calc_md5
+    size=$(zip_get_size "$zip" "$file") || exit_upgrade "get size $zip $file"
+    read_md5=$(zip_read_md5 "$zip" "$file") || exit_upgrade "read md5 $zip $file"
+    step_log "Write $part with $file size: $size"
+    echo "$size" >"$ResultFile.$file.size"
+
+    local tmpfile=$(mktemp)
+    unzip -p "$zip" "$file" 2>/dev/null | tee >(md5sum >"$tmpfile") | dd of="$part" bs=1M status=progress 2>&1 | tee "$ResultFile.$file.status"
+    local ret=$?
+    calc_md5=$(cat $tmpfile | awk '{print $1}')
+    rm -rf $tmpfile
+    [ $ret -ne 0 ] && exit_upgrade "write $part with $file"
+
+    step_log "Check md5sum $part"
+    [ "$calc_md5" = "$read_md5" ] || exit_upgrade "check md5sum $part"
 }
 
-check_zip() { # ota.zip
-    local dir=$1 md5file=$dir/$MD5_FILE
-    [ -s $md5file ] || exit_upgrade "$md5file is empty"
-    file=$dir/$(parse_md5 name "$md5file")
-    [ -s $file ] || exit_upgrade "$file is empty"
-    md5=$(dd_calc_md5 "$file")
-    [ "$md5" != "$(parse_md5 md5 "$md5file")" ] && exit_upgrade "md5 mismatch"
+dd_calc_md5() {
+    DD_Calc_MD5=""
+    local file=$1 size=$2
+    [ -e "$file" ] || exit_upgrade "file $file not exist"
+    [ -z "$size" ] && size=$(stat -c %s "$file" 2>/dev/null) || size=$2
+    [ $((size)) -eq 0 ] && exit_upgrade "unknown size $file"
+    local tmpfile=$(mktemp)
+    dd if="$file" bs=1M status=progress | head -c $size | md5sum >"$tmpfile"
+    local ret=$?
+    DD_Calc_MD5=$(cat $tmpfile | awk '{print $1}')
+    rm -f $tmpfile
+    [ $ret -ne 0 ] && exit_upgrade "calc md5sum $file"
 }
 
 ota_boot() { # fip.bin boot.emmc
     local zip=$1 part=$2 file=$3
     local size read_md5 calc_md5
-    log_step "Check $part and $file"
-    size=$(zip_get_size $zip $file) || exit_upgrade "get size $file"
-    read_md5=$(zip_read_md5 $zip $file) || exit_upgrade "read md5 $file"
-    calc_md5=$(dd_calc_md5 $part $size) || exit_upgrade "calc md5 $part"
-    [ "$read_md5" = "$calc_md5" ] && { echo "Skiped with md5 match"; } && { return 1; }
+    step_log "Check $part and $file"
+    size=$(zip_get_size $zip $file)
+    read_md5=$(zip_read_md5 $zip $file)
+    ([ -z "$size" ] || [ -z "$read_md5" ]) && {
+        step_result "skip with no valid $file"
+        return 0
+    }
 
-    calc_md5=$(zip_calc_md5 $zip $file) || exit_upgrade "calc md5 $part"
-    [ "$part" = "$FIP_PART" ] && echo 0 >/sys/block/mmcblk0boot0/force_ro
-    [ "$read_md5" = "$calc_md5" ] && zip_write_part $zip $part $file
-    [ "$part" = "$FIP_PART" ] && echo 1 >/sys/block/mmcblk0boot0/force_ro
+    dd_calc_md5 $part $size
+    calc_md5=$DD_Calc_MD5
+    [ "$read_md5" = "$calc_md5" ] && {
+        step_result "skip with md5 matched"
+        return 0
+    }
+
+    calc_md5=$(unzip -p $zip $file 2>/dev/null | md5sum | awk '{print $1}')
+    [ "$calc_md5" != "$read_md5" ] && {
+        step_result "skip with $file damaged"
+        return 0
+    }
+
+    [ "$part" = "$FIP_PART" ] && echo 0 >"/sys/block/mmcblk0boot0/force_ro" 2>/dev/null
+    zip_write_part $zip $part $file
+    [ "$part" = "$FIP_PART" ] && echo 1 >"/sys/block/mmcblk0boot0/force_ro" 2>/dev/null
 }
 
 #####
-latest() {
-    RUN_CASE=$1
+latest_cmd() {
     local cmd=$2
-    local file_result="$UPGRADE_FILES/$RUN_CASE"
-    [ "$2" = "q" ] && {
-        [ -f $file_result ] && cat $file_result
+    [ "$cmd" = "q" ] && {
+        [ -f $ResultFile ] && cat $ResultFile
         exit 0
     }
     [ "$cmd" = "x" ] && ps_stop
-    [ -z "$2" ] && {
-        echo "Usage: $0 $RUN_CASE <url> | [q]"
-        exit 1
-    }
-    ps_mutex
+}
 
+latest() {
+    latest_cmd $@
+    ps_mutex
     local url="$2"
-    log_step "Parse $url"
+    [ -z "$url" ] && url="$OFFICIAL_URL"
+    step_log "Parse $url"
+    local md5_url md5_path
     md5_url=$(get_upgrade_url "$url") || exit_upgrade "parse $url"
+    step_result "$md5_url"
+
     md5_path="$UPGRADE_FILES/$MD5_FILE"
     rm -f "$md5_path"
     wget_file "$md5_url" "$md5_path"
@@ -235,37 +247,34 @@ latest() {
     local os_name version
     os_name=$(parse_md5 os "$md5_path") || exit_upgrade "parse os $md5_path"
     version=$(parse_md5 version "$md5_path") || exit_upgrade "parse version $md5_path"
-    echo "$os_name $version" >$file_result
+    echo "$os_name $version" >$ResultFile
     echo "$(echo ${md5_url%/*})" >"$UPGRADE_FILES/$URL_FILE"
-
-    log_step "Result $os_name@$version"
+    step_result "$os_name@$version"
     exit_upgrade
 }
 
-download() {
-    RUN_CASE=$1
+download_cmd() {
     local cmd=$2
-    local file_result="$UPGRADE_FILES/$RUN_CASE"
     [ "$cmd" = "q" ] && {
-        [ -f $file_result ] && {
-            cat $file_result
+        [ -f $ResultFile ] && {
+            cat $ResultFile
             exit 0
         }
-        local file=$(cat "$file_result.file" 2>/dev/null)
-        local size=$(cat "$file_result.size" 2>/dev/null)
-        [ $((size)) -ne 0 ] && {
-            local dl_size=$(stat -c %s "$file")
-            dl_size=$(($dl_size * 100))
-            dl_size=$(($dl_size / $size))
-            echo "$dl_size"
-        }
+        local total=$(cat "$ResultFile.size" 2>/dev/null)
+        local file=$(cat "$ResultFile.file" 2>/dev/null)
+        local size=$(stat -c %s "$file")
+        echo "$((size)) $((total))"
         exit 0
     }
     [ "$cmd" = "x" ] && ps_stop
     [ ! -z "$cmd" ] && {
-        echo "Usage: $0 $RUN_CASE [q]"
+        echo "Usage: $0 $RunCase [q]"
         exit 1
     }
+}
+
+download() {
+    download_cmd $@
     ps_mutex
     mount_recovery
 
@@ -273,74 +282,81 @@ download() {
     local url_path_tmp="$tmpdir/$URL_FILE"
     local md5_path_tmp="$tmpdir/$MD5_FILE"
     local md5_path_latest="$UPGRADE_FILES/$MD5_FILE"
-    local md5_path_now="$MountPath/$MD5_FILE"
 
-    log_step "Check files"
+    step_log "Check files"
     [ -s $md5_path_latest ] && {
+        local md5_path_now="$MountPath/$MD5_FILE"
         [ -s "$md5_path_now" ] && {
             [ -z "$(diff "$md5_path_latest" "$md5_path_now")" ] && {
-                echo "OTA is up to date."
+                step_result "OTA is up to date."
                 exit_upgrade
             }
         }
         ([ ! -s "$md5_path_tmp" ] || [ ! -z "$(diff "$md5_path_latest" "$md5_path_tmp")" ]) && {
-            log_step "Copy latest files"
+            step_log "Copy latest files"
             rm -rf $tmpdir/*
             cp -f "$md5_path_latest" "$md5_path_tmp"
             cp -f "$UPGRADE_FILES/$URL_FILE" "$url_path_tmp"
         }
     }
+    ([ -s "$md5_path_tmp" ] && [ -s "$url_path_tmp" ]) || {
+        exit_upgrade "skip with no latest files, please run 'upgrade.sh latest [url]' first"
+    }
 
-    ([ -s "$md5_path_tmp" ] && [ -s "$url_path_tmp" ]) || exit_upgrade "run 'upgrade.sh latest' first"
-
-    local filename url
+    local filename md5 url
     filename=$(parse_md5 name "$md5_path_tmp") || exit_upgrade "parse name $md5_path_tmp"
+    md5=$(parse_md5 md5 "$md5_path_tmp") || exit_upgrade "parse md5 $md5_path_tmp"
     url=$(cat "$url_path_tmp")/$filename
     wget_file "$url" "$tmpdir/$filename"
 
-    log_step "Check $filename md5sum"
-    check_zip "$tmpdir"
+    step_log "Check md5sum $filename"
+    dd_calc_md5 "$tmpdir/$filename"
+    [ "$md5" != "$DD_Calc_MD5" ] && exit_upgrade "md5 mismatch"
 
-    log_step "Sync files"
+    step_log "Sync files"
     rm -rf $MountPath/*
     cp -f $tmpdir/*.zip $MountPath/ || exit_upgrade "copy $tmpdir/*.zip"
     cp -f $tmpdir/*.txt $MountPath/ || exit_upgrade "copy $tmpdir/*.txt"
     rm -rf $tmpdir
 
-    # EXIT
     exit_upgrade
 }
 
-start() {
-    RUN_CASE=$1
+start_cmd() {
     local cmd=$2
-    local file_result="$UPGRADE_FILES/$RUN_CASE"
-
     [ "$cmd" = "x" ] && ps_stop
-    [ "$2" = "q" ] && { [ -f "$file_result" ] && { cat "$file_result"; } && exit 0; }
+    [ "$cmd" = "q" ] && {
+        [ -f "$ResultFile" ] && {
+            cat "$ResultFile"
+            exit 0
+        }
+        local size=$(tail "$ResultFile.rootfs_ext4.emmc.status" 2>/dev/null | awk '{print $1}')
+        local total=$(cat "$ResultFile.rootfs_ext4.emmc.size" 2>/dev/null)
+        echo "$((size)) $((total))"
+        exit 0
+    }
+}
+
+start() {
+    start_cmd $@
     ps_mutex
     mount_recovery
 
-    zip="$2"
-    log_step "Check ota pack $zip"
-    [ ! -s "$zip" ] && { zip="$MountPath/$(parse_md5 name "$MountPath/$MD5_FILE")"; }
-    [ ! -s "$zip" ] && { exit_upgrade "not found ota.zip"; }
-    log_step "OTA will use $zip"
+    local zip="$2"
+    step_log "Check ota pack $zip"
+    [ -z "$zip" ] && {
+        file="$(parse_md5 name "$MountPath/$MD5_FILE")" || exit_upgrade "parse name $MountPath/$MD5_FILE"
+        zip="$MountPath/$file"
+    }
+    [ -e "$zip" ] || { exit_upgrade "not found $zip"; }
+    step_result "OTA will use $zip"
 
     ota_boot "$zip" "$FIP_PART" "fip.bin"
     ota_boot "$zip" "$BOOT_PART" "boot.emmc"
 
     local target="$ROOTFS_B"
-    is_use_partition_b
-    [ $? -eq 1 ] && target="$ROOTFS"
-    zip_write_part "$zip" "$target" "$ROOTFS_FILE"
-
-    log_step "Check md5 $target"
-    local calc_md5="" read_md5="" size=""
-    size=$(zip_get_size "$zip" "$ROOTFS_FILE") || exit_upgrade "get size $zip $ROOTFS_FILE"
-    read_md5=$(zip_read_md5 "$zip" "$ROOTFS_FILE") || exit_upgrade "read md5 $zip $ROOTFS_FILE"
-    calc_md5=$(dd_calc_md5 $target $size) || exit_upgrade "calc_md5 $target $size"
-    [ "$read_md5" == "$calc_md5" ] || exit_upgrade "md5 mismatch $target"
+    is_rootfs_b || target="$ROOTFS"
+    zip_write_part "$zip" "$target" "rootfs_ext4.emmc"
 
     switch_partition
     exit_upgrade
@@ -352,9 +368,9 @@ rollback() {
 }
 
 recovery() {
-    fw_setenv factory_reset 1
-    [ $? -ne 0 ] && { exit_upgrade "recovery"; }
-    echo "Please reboot."
+    step_log "Set recovery flag"
+    set_bootenv factory_reset 1
+    step_log "Please reboot to take effect."
     exit_upgrade
 }
 
@@ -365,4 +381,13 @@ clean() {
 }
 
 # call function
-[ "$(type $1)" = "$1 is a function" ] && { $1 $@; } || { echo "Not found"; }
+RunCase=$1
+ResultFile="$UPGRADE_FILES/$RunCase"
+FUNCS=(latest download start rollback recovery clean)
+for func in ${FUNCS[@]}; do
+    [ "$RunCase" == "$func" ] && {
+        $func $@
+        exit 0
+    }
+done
+echo "Not found $RunCase"
