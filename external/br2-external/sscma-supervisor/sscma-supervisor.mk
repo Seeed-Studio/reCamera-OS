b6f57c3f6f6b62659811cde0da47acac60281e4d
Lesords <lesords168@gmail.com>
2026-01-27 09:03:50 +0000

    chore: update supervisor to version 0.2.3

--- external/br2-external/sscma-supervisor/sscma-supervisor.mk
+++ external/br2-external/sscma-supervisor/sscma-supervisor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SSCMA_SUPERVISOR_VERSION = 0.2.2
+SSCMA_SUPERVISOR_VERSION = 0.2.3
 SSCMA_SUPERVISOR_SITE = https://github.com/Seeed-Studio/sscma-example-sg200x
 SSCMA_SUPERVISOR_SITE_METHOD = git
 SSCMA_SUPERVISOR_GIT_SUBMODULES = YES
